7ce2bda32695a34aeaa89e8130cf47ce0b57a301
Akash <your-email@example.com>
2025-09-18 06:09:51 +0530

    createusermodel

---
+++ src/models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const userSchema = new Schema({
+username:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true,
+    trim:true,
+    index:true
+
+},
+email:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true,
+    trim:true,
+
+},
+fullname:{
+    type:String,
+    required:true,
+    trim:true,
+    index:true
+
+},
+avatar:{
+
+    type:String,// coudinary url
+    required:true,
+
+    
+},
+coverImage:{
+    type:String,// coundinary url
+}
+})
+
+
+export const User = mongoose.model("User" , userSchema)
